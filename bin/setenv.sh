554e5aacbceb5e80e08627d4de190e02d8a17f24
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2016-03-29 23:26:54 +0300

    v3.2.2

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -4,6 +4,6 @@ export WEBITEL_SSL="off"
 export WEBITEL_PROTO="http"
 export WEBITEL_HOST="webitel.lo"
 export WEBITEL_PASS="secret"
-export WEBITEL_VERSION="3.2.1"
+export WEBITEL_VERSION="3.2.2"
 export WEBITEL_DIR="/opt/webitel"
 export WEBITEL_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
