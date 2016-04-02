e5a88a3c10cd2ca725e20e3ef8e8259d6ab3f9da
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2016-04-02 21:38:22 +0300

    added backup scrips

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -24,12 +24,20 @@ case "$1" in
         printf "Webitel developers stack\n\n"
         $DC -p webitel -f "${DIR}/dev/docker-compose.yml" $2 $3 $4
         ;;
+    "backup")
+        $DC -p webitel -f "${DIR}/misc/docker-compose.yml" up backupDB
+        $DC -p webitel -f "${DIR}/misc/docker-compose.yml" rm -f backupDB
+        tar -cvzf ${WEBITEL_DIR}/backup/backup_$TIMESTAMP.tar.gz "${WEBITEL_DIR}/ssl" "${WEBITEL_DIR}/db" "${WEBITEL_DIR}/elasticsearch" "${WEBITEL_DIR}/mongodb_backup/dump"
+        rm -rf ${WEBITEL_DIR}/mongodb_backup/dump
+        find ${WEBITEL_DIR}/backup/ -maxdepth 1 -mtime +$BACKUP_LIFETIME_DAYS -type f -exec rm {} \;
+        ;;
     "fs")
         docker run -i --rm -t "webitel/freeswitch:${WEBITEL_VERSION}" fs_cli -H 172.17.0.1
         ;;
     "help")
         echo "esf - Elasticsearch and fluent containers"
         echo "dev - Webitel containers in the development mode"
+        echo "backup - Backup webitel files"
         echo "fs - Run fs_cli container"
         exit 1
         ;;
--- bin/setenv.sh
+++ bin/setenv.sh
@@ -7,3 +7,5 @@ export WEBITEL_PASS="secret"
 export WEBITEL_VERSION="3.2.2"
 export WEBITEL_DIR="/opt/webitel"
 export WEBITEL_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+export TIMESTAMP=`date "+%Y-%m-%d"`
+export BACKUP_LIFETIME_DAYS="5"
