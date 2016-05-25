d42925c2d0feabf499a4c718c416a4d0d0cfd933
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2016-05-25 19:53:49 +0300

    fixed broker string

--- bin/test.sh
+++ bin/test.sh
@@ -5,9 +5,14 @@ set -e
 sleep 1m
 
 do_test=$(curl --silent -X POST -H 'Content-Type: application/json' -d '{"username":"root","password":"secret"}' "http://${WEBITEL_HOST}/engine/login")
+user_tst=$(echo $do_test | jq '. | .username' )
 
 if [ -z "$do_test" ]; then
     exit 1
 fi
 
-echo $do_test
+if [[ $user_tst != *root* ]]; then
+    exit 1
+fi
+
+echo $do_test | jq .
