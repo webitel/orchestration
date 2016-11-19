92140cdae81ff5a69fe517c0c42df9ef2fea9400
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2016-11-20 00:54:43 +0200

    Replace "rm -f" with "down -v" for cleans up everything correctly including all volumes

--- bin/reboot.sh
+++ bin/reboot.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/setenv.sh
 
-$DC -p webitel -f "${DIR}/srv/docker-compose.yml" rm -f
+$DC -p webitel -f "${DIR}/srv/docker-compose.yml" down -v
 $DC -p webitel -f "${DIR}/srv/docker-compose.yml" up -d
