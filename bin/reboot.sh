e03c9f003017971e0f6c0cba5be6ae71b4539bcc
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2017-03-01 15:02:23 +0200

    webitel docker-compose config

--- bin/reboot.sh
+++ bin/reboot.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/setenv.sh
 
-$DC -p webitel -f "${DIR}/srv/docker-compose.yml" down -v
-$DC -p webitel -f "${DIR}/srv/docker-compose.yml" up -d
+$DC -p webitel -f "${DIR}/${WEBITEL_DC}/docker-compose.yml" down -v
+$DC -p webitel -f "${DIR}/${WEBITEL_DC}/docker-compose.yml" up -d
