7cedc11a9f0b5130940fc6e90e43d12e71e4780a
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2017-05-01 16:35:45 +0300

    Remove all unused volumes

--- bin/reboot.sh
+++ bin/reboot.sh
@@ -5,3 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $DC -p webitel -f "${DIR}/${WEBITEL_DC}/docker-compose.yml" down
 $DC -p webitel -f "${DIR}/${WEBITEL_DC}/docker-compose.yml" up -d
+
+docker volume prune -f
